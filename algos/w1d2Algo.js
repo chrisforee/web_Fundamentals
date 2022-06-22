22360555be70c9f0bab58b03dd311f62dcb8f5d8
chrisforee <chrisforee@gmail.com>
2022-06-22 11:28:38 -0500

    added from demo

---
+++ algos/w1d2Algo.js
@@ -0,0 +1,39 @@
+
+var name= "chris foree";
+var age= 29; 
+const numDayWeek = 7
+var chipoltePrice= 9.75;
+var isTeachingToday= true;
+
+console.log("wazaaaaa" + name);
+
+chipoltePrice -= 1;
+
+console.log(chipoltePrice);
+
+var halfMyAge = age/2;
+console.log(halfMyAge);
+var twiceMyAge = age*2;
+console.log(twiceMyAge);
+console.log("half my age is", halfMyAge);
+
+
+
+//camelCase
+//snake_case
+//PascalCase
+
+
+//Data Structures
+//1. Array []
+var artistsArray = ["Drake", "weezy", "Weekend", "NIN", "Saint John", "Taylor Swift", "Weezer", "Andre3000", "Billie"];
+
+//indexes    
+
+console.log(artistsArray[5]);
+
+// A way to repeat a process a certain number of times
+for(var i = 0; i < artistsArray.length; i++){
+    console.log(artistsArray[i]);
+}
+
