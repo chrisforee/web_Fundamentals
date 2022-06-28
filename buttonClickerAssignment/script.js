f4cd02be4476592e97667774a7572eccf9e98b67
chrisforee <chrisforee@gmail.com>
2022-06-27 22:50:14 -0500

    added buttonClicker

---
+++ buttonClickerAssignment/script.js
@@ -0,0 +1,8 @@
+function changeStatus(statusElement){
+    console.log("Change status passed");
+    statusElement.innerText = "Logout";
+}
+function removeElement(buttonElement){
+    console.log("Removal passed");
+    buttonElement.remove();
+}
